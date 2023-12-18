ed43392fd973a5fa687ce02cc2ab99b045577980
unknown <moaaztarek623@gmail.com>
2023-12-18 20:14:11 +0200

    add (Components) folder and (global) folder to setup route-dom

---
+++ src/global/SideBar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { tokens } from '../theme';
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+const SideBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+      <Sidebar collapsed={isCollapsed} backgroundColor={`${colors.primary[400]}`} style={{
+        border: "transparent !important"
+      }}>
+        <Menu iconShape="square" menuItemStyles={{
+          subMenuContent:{
+            background: "red"
+          }
+        }}>
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+        </Menu>
+      </Sidebar>
+  );
+}
+
+export default SideBar
