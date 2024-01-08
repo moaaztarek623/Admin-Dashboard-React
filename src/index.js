9da22ce8a2c949dec7e6a04c0bd97bd1b2e5ee32
unknown <moaaztarek623@gmail.com>
2024-01-09 01:35:43 +0200

    deploy app

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='Admin-Dashboard-React'>
       <App />
     </BrowserRouter> 
   </React.StrictMode>
