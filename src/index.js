84b9f92e9497f2e4c06054ae6813f3f343f84566
unknown <moaaztarek623@gmail.com>
2024-01-09 20:17:21 +0200

    some edits

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='Admin-Dashboard-React'>
+    <BrowserRouter>
       <App />
     </BrowserRouter> 
   </React.StrictMode>
